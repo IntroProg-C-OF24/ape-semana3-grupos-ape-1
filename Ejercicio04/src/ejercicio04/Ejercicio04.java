6e58d87018508c19a9416353d31e81643d9f35bd
JU4NSCV <jscueva5@utpl.edu.ec>
2023-10-26 17:27:53 -0500

    Cuarto ejercicio

--- Ejercicio04/src/ejercicio04/Ejercicio04.java
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -1,7 +1,16 @@
 package ejercicio04;
 public class Ejercicio04 {
 public static void main(String[] args) {
-        
+        int var=20;
+        int var1=1;
+        int var2=3;
+        int var3=2;
+        int var4=3;
+        boolean respuesta;
+        int rest;
+        respuesta = ((var)/(var1+var2))+var3 >= var4;
+        rest = ((var)/(var1+var2))+var3;
+        System.out.println("La Respuesta es: " + rest + " asi que es: " + respuesta);
     }
     
 }
