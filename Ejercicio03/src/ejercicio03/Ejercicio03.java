457f5cbd6584f4d43733b1a8b41d2220dcf375ad
NicolayJ <dnjimenez6@utpl.edu.ec>
2023-10-26 17:29:36 -0500

    Ejercicio 03

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio03;
-
-/**
- *
- * @author reroes
- */
 public class Ejercicio03 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        //Resolucion del problema 03
+        int var1 = 10;
+        int var2 = 2;
+        int var3 = 16;
+        int var4 = 20;
+        int var5 = 1;
+        int var6 = 11;
+        //Planteamiento de la formula
+        double resultado = Math.pow(var1, var2) + Math.sqrt(var3) + (double)var4/(var5 + var6);
+        System.out.println("El resultado es: " + resultado);
+        //Hecho por Diego Jimenez.
     }
     
 }
