d962f2e91b8a522481331367b87a29bbf45a5ebc
NicolayJ <dnjimenez6@utpl.edu.ec>
2023-10-31 21:43:53 -0500

    Ejercicios

--- Ejercicio07/src/ejercicio07/Ejercicio07.java
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -10,27 +10,26 @@ public class Ejercicio07 {
         int var5 = 1;
         int var6 = 100;
         int var7 = 25;
+        int var8 = 0;
         boolean op1;
         boolean op2;
-        boolean rta = true;
+        boolean op3;
                 
-       // Planteamiento de la formula
+       // Planteamiento de la formula-
        op1 = ((Math.sqrt(var1)+ var2)/ var3) == var2;
        op2 = (var4 > var5);
+       op3 = ((var6/var7)+(Math.sqrt(var6))== var8);
        
-       double resultado  = ((var6/var7)+(Math.sqrt(var6)));
+       boolean resultado  = ((op1 || op2) && op3);
        
                
         // Impresion de cada solucion
         System.out.println("La respuesta a la primera operacion es: " + op1);
         System.out.println("La respuesta a la segunda operacion es: " + op2);
-        System.out.println("La respuesta de las 2 primeras operaciones es: " + rta);
-        System.out.println("La respuesta a la tercera opcione es: " + resultado);
+        System.out.println("La respuesta a la tercera operacion es: " + op3);
+        System.out.println("La respuesta final es: " + resultado);
         
-        // El ejercicio 7 no tiene solucion debido a que la tercera opcion tiene un valor numerico y las demas opciones tienen resultados booleanos por lo tanto no se puede operar.
+        // El ejercicio 7 en base al problema planteado no tiene solucion sin embargo se coloco al final de la operacion ==0 para poder realizarlo asi logrando resolverlo y obteniendo resultados booleanos
         // Hecho por Diego Jimenez
     }    
-       
-    
-    
 }
